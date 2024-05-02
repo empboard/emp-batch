8f372a94b2091e83be64966343a8a4157cce3c82
rivercity310 <h970126@gmail.com>
2024-05-02 23:05:21 +0900

    Initialize Repository

---
+++ src/test/java/com/empbatchserver/EmpBatchServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.empbatchserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpBatchServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
